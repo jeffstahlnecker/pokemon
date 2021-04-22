8ee3cb8f6b4b2337d9cab27400283fcba0eacea7
Jeff Stahlnecker <45363541+jeffstahlnecker@users.noreply.github.com>
2021-04-22 08:37:39 +0200

    move class to another file

--- bin/pokemon.dart
+++ bin/pokemon.dart
@@ -3,68 +3,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-class Pokemon {
-  List abilities;
-  int base_experience;
-  List forms;
-  List game_indices;
-  int height;
-  List held_items;
-  int id;
-  bool is_default;
-  String location_area_encounters;
-  List moves;
-  String name;
-  int order;
-  List past_types;
-  Map species;
-  Map sprites;
-  List stats;
-  List types;
-  int weight;
-
-  Pokemon(
-      {this.abilities,
-      this.base_experience,
-      this.forms,
-      this.game_indices,
-      this.height,
-      this.held_items,
-      this.id,
-      this.is_default,
-      this.location_area_encounters,
-      this.moves,
-      this.name,
-      this.order,
-      this.past_types,
-      this.species,
-      this.sprites,
-      this.stats,
-      this.types,
-      this.weight});
-
-  factory Pokemon.fromJson(Map<String, dynamic> json) {
-    return Pokemon(
-        abilities: json['abilities'],
-        base_experience: json['base_experience'],
-        forms: json['forms'],
-        game_indices: json['game_indices'],
-        height: json['height'],
-        held_items: json['held_items'],
-        id: json['id'],
-        is_default: json['is_default'],
-        location_area_encounters: json['location_area_encounters'],
-        moves: json['moves'],
-        name: json['name'],
-        order: json['order'],
-        past_types: json['past_types'],
-        species: json['species'],
-        sprites: json['sprites'],
-        stats: json['stats'],
-        types: json['types'],
-        weight: json['weight']);
-  }
-}
+import 'pokemon_class.dart';
 
 void main() async {
   Future<Pokemon> fetchPokemon() async {
